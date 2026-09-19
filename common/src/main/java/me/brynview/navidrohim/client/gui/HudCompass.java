d4a14d2481cbaa5f4b0285bb3e2487acadd8d718
Navid <navidanjumsamirohim@gmail.com>
2026-09-19 18:10:57 +0100

    Compass width can be dynamically resized. Changed background design and entity blip. Compass visibility can be bad in certain conditions

--- common/src/main/java/me/brynview/navidrohim/client/gui/HudCompass.java
+++ common/src/main/java/me/brynview/navidrohim/client/gui/HudCompass.java
@@ -1,5 +1,6 @@
 package me.brynview.navidrohim.client.gui;
 
+import me.brynview.navidrohim.Constants;
 import me.brynview.navidrohim.util.ColorHelper;
 import me.brynview.navidrohim.util.FontHelper;
 import me.brynview.navidrohim.util.MathHelper;
@@ -9,22 +10,25 @@ import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.phys.AABB;
-import net.minecraft.world.phys.Vec2;
 import net.minecraft.world.phys.Vec3;
 
 public class HudCompass
 {
-    private static final int CENTER_COLOR = ColorHelper.decode("#b02e26").getRGB();
+    private static final int CENTER_COLOR = ColorHelper.decode("#FF0000").getRGB();
+    private static final int COMPASS_BG_COLOR = ColorHelper.rgb(255, 255, 255, 155);
+    private static final int COMPASS_BG_SHADOW_COLOUR = ColorHelper.rgb(0, 0, 0, 100);
 
-    private static final String COMPASS_CENTER = "|";
-    private static final String ENTITY_LABEL = "·";
+    private static final String COMPASS_HEADING = "|";
+    private static final String ENTITY_LABEL = "⏺";
 
     static int SCALE_MAX = 255;
     static int DETECTION_DISTANCE = 40;
     static int MAX_ALLOWED_ENTITIES_ON_COMPASS = 10;
 
+    static int sizePercentageOfScaledWidth = 70;
+
     // value at lowest fov (30): 3.477 blocks value at highest (110): 12.75
-    static double EXPAND_INFLATE_WITH_FOV_CONST = 0.115909;
+    static double EXPAND_INFLATE_WITH_FOV_CONST = 0.118909;
 
     public static void drawState(GuiGraphicsExtractor guiGraphics, Minecraft mc, int scaledWidth, float _partialTicks) {
         final LocalPlayer player = mc.player;
@@ -35,31 +39,38 @@ public class HudCompass
 
         final float partialTicks = mc.isPaused() ? 0 : _partialTicks;
 
-        final int x = MathHelper.getCompassX(scaledWidth, mc.font.width(COMPASS_CENTER));
+        final int x = MathHelper.getCompassX(scaledWidth, mc.font.width(COMPASS_HEADING));
+        final int compassScaledWidth = (int) (((float) sizePercentageOfScaledWidth / 100f) * (float) scaledWidth);
+        final int compassScaledWidthHalf = compassScaledWidth / 2;
+
         final int y = MathHelper.getCompassY();
         final float yaw = (!player.isPassenger() ?
                 Mth.lerp(partialTicks, player.yRotO, player.getYRot())
                 : player.getYRot()) % 360;
 
-        final int bgColor = ColorHelper.rgb(0, 0, 0, 100);
-
         // Compass background
-        guiGraphics.fill(x - 92, y - 1, x + 96, y + mc.font.lineHeight - 1, bgColor);
+        drawBackground(guiGraphics, mc, compassScaledWidthHalf, x, y);
 
         // Render N/E/S/W
-        drawCardinal(mc, guiGraphics, yaw, 0, x, y, "S");
-        drawCardinal(mc, guiGraphics, yaw, 90, x, y, "W");
-        drawCardinal(mc, guiGraphics, yaw, 180, x, y, "N");
-        drawCardinal(mc, guiGraphics, yaw, 270, x, y, "E");
+        drawCardinal(mc, guiGraphics, yaw, 0, x, y, compassScaledWidth, "S", scaledWidth);
+        drawCardinal(mc, guiGraphics, yaw, 90, x, y, compassScaledWidth, "W", scaledWidth);
+        drawCardinal(mc, guiGraphics, yaw, 180, x, y, compassScaledWidth, "N", scaledWidth);
+        drawCardinal(mc, guiGraphics, yaw, 270, x, y, compassScaledWidth, "E", scaledWidth);
 
         // Draw all living entities
-        drawEntities(mc, player, guiGraphics, partialTicks, yaw, x, y);
+        drawEntities(mc, player, guiGraphics, partialTicks, yaw, x, y, compassScaledWidthHalf, scaledWidth);
 
         // Draw compass heading
-        FontHelper.draw(mc, guiGraphics, COMPASS_CENTER, x, y, CENTER_COLOR, false, FontHelper.TextType.NONE);
+        FontHelper.draw(mc, guiGraphics, COMPASS_HEADING, x, y, CENTER_COLOR, false, FontHelper.TextType.NONE);
     }
 
-    private static void drawEntities(Minecraft mc, LocalPlayer player, GuiGraphicsExtractor guiGraphics, float partialTicks, float yaw, int compassX, int compassY)
+    private static void drawBackground(GuiGraphicsExtractor guiGraphicsExtractor, Minecraft mc, int compassScaledWidthHalf, int x, int y)
+    {
+        guiGraphicsExtractor.horizontalLine(x - compassScaledWidthHalf, x + compassScaledWidthHalf, (y + mc.font.lineHeight - 1) / 2, COMPASS_BG_COLOR);
+        guiGraphicsExtractor.horizontalLine((x - compassScaledWidthHalf) + 1, (x + compassScaledWidthHalf) + 1, ((y + mc.font.lineHeight - 1) / 2) + 1, COMPASS_BG_SHADOW_COLOUR);
+    }
+
+    private static void drawEntities(Minecraft mc, LocalPlayer player, GuiGraphicsExtractor guiGraphics, float partialTicks, float yaw, int compassX, int compassY, int compassScaledWidth, int screenScaledWidth)
     {
         // Where player is looking
         Vec3 look = player.getViewVector(partialTicks).scale(DETECTION_DISTANCE);
@@ -67,7 +78,7 @@ public class HudCompass
         // Get bounding box 40m infront of where player is looking
         int fov = mc.options.fov().get();
 
-        AABB normalBB = player.getBoundingBox().inflate(1);
+        AABB normalBB = player.getBoundingBox();
         AABB inFrontOfPlayer = normalBB
                 .expandTowards(look.x, 0, look.z)
                 .inflate(EXPAND_INFLATE_WITH_FOV_CONST * fov); // inflate so entities don't get cut off from compass
@@ -80,14 +91,14 @@ public class HudCompass
                     int iconScale = Math.abs(SCALE_MAX - (SCALE_MAX / DETECTION_DISTANCE * distanceFromEntity));
 
                     // Entity x offset on screen
-                    int ex = MathHelper.getCompassScreenX(yaw, (float) angleFromEntity, compassX, 1);
-                    FontHelper.draw(mc, guiGraphics, ENTITY_LABEL, ex, compassY, ColorHelper.rgb(255, 255, 255, iconScale), false, FontHelper.TextType.NONE);
+                    int ex = MathHelper.getCompassScreenX(yaw, (float) angleFromEntity, compassX, compassScaledWidth, screenScaledWidth);
+                    FontHelper.draw(mc, guiGraphics, ENTITY_LABEL, ex, compassY, ColorHelper.rgb(255, 255, 255, iconScale), true, FontHelper.TextType.NONE);
                 }
         );
     }
 
-    private static void drawCardinal(Minecraft mc, GuiGraphicsExtractor guiGraphics, float yaw, float angle, int x, int y, String text) {
-        int dx = MathHelper.getCompassScreenX(yaw, angle, x, 1.0F);
+    private static void drawCardinal(Minecraft mc, GuiGraphicsExtractor guiGraphics, float yaw, float angle, int x, int y, int compassScaledWidth, String text, int screenScaledWidth) {
+        int dx = MathHelper.getCompassScreenX(yaw, angle, x, compassScaledWidth, screenScaledWidth);
         FontHelper.draw(mc, guiGraphics, text, dx, y, ColorHelper.decode("#FFFFFF").getRGB(), FontHelper.TextType.NONE);
     }
 }
